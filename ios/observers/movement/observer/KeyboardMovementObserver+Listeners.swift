a6b8eca463b164f493d0a2f61dd4de72fce07a44
kirillzyusko <zyusko.kirik@gmail.com>
2026-07-03 11:37:10 +0200

    chore: debugging code

--- ios/observers/movement/observer/KeyboardMovementObserver+Listeners.swift
+++ ios/observers/movement/observer/KeyboardMovementObserver+Listeners.swift
@@ -7,6 +7,7 @@
 
 extension KeyboardMovementObserver {
   @objc func keyboardWillAppear(_ notification: Notification) {
+    print("keyboardWillAppear")
     guard !UIResponder.isKeyboardPreloading else { return }
 
     let (duration, frame) = notification.keyboardMetaData()
@@ -34,6 +35,7 @@ extension KeyboardMovementObserver {
   }
 
   @objc func keyboardWillDisappear(_ notification: Notification) {
+    print("keyboardWillDisappear")
     guard !UIResponder.isKeyboardPreloading else { return }
     let (duration, _) = notification.keyboardMetaData()
     tag = UIResponder.current.reactViewTag
@@ -99,6 +101,10 @@ extension KeyboardMovementObserver {
 
   @objc func scheduleDidEvent(height: CGFloat, duration: CGFloat) {
     keyboardDidTask?.cancel()
+    
+    print("scheduleDidEvent")
+    print(height)
+    print(duration)
 
     guard let notification = notification else {
       return
